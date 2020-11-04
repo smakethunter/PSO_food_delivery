8adb9540c4fb4bc771a56e4486f3ad84de98f5be
smaket <wojtek.pelka@gmail.com>
2020-11-04 20:52:03 +0100

    dodano szkielet PSO

--- utils.py
+++ utils.py
@@ -2,44 +2,29 @@ from abc import ABC, abstractmethod
 from typing import List
 
 
-class Individual(ABC):
-    @abstractmethod
-    def pair(self):
-        pass
+class Particle(ABC):
 
     @abstractmethod
-    def mutate(self):
+    def move(self):
         pass
 
     @abstractmethod
     def fitness(self):
         pass
-    pass
 
 
-class Population:
-    def __init__(self, _population: List[Individual]):
-        self.population = _population
 
-    def get_parents(self, nr_offsprings):
-        self.population.sort(key=lambda x: x.fitness())
-        mothers = self.population[len(self.population)-2*nr_offsprings: len(self.population)-nr_offsprings]
-        fathers = self.population[len(self.population)-nr_offsprings::]
-        return [(mother, father) for mother, father in zip(mothers, fathers)]
+
+class Swarm:
+
+    def __init__(self, starting_position):
+        self.best_position = starting_position
+        self.swarm: List[Particle] = starting_position
+        self.fitness = self.fitness()
+
+    def fitness(self):
+        fitness = 0
+        for x in self.swarm:
+            fitness += x.fitness()
+            return fitness
         pass
-
-    def change_population(self, nr_offspring):
-
-        pass
-
-    pass
-
-
-class Evolution:
-    def make_offspring(self):
-        pass
-
-    def evolution_step(self):
-        pass
-    pass
-
